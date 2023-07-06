07a6555bbfa4fc683e1f33ef3b84400ef553b7b1
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-07-06 10:54:07 -0300

    Carregando noticias

--- UC9SA3-ProjetoAngular-main/src/app/views/home/home.component.ts
+++ UC9SA3-ProjetoAngular-main/src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from 'src/app/services/noticias.service';
+import { Noticias } from 'src/app/models/noticias';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  constructor(private noticiaService: NoticiasService) { }
+
+  listaNoticias = [] as Noticias []
 
   ngOnInit(): void {
+
+    this.carregarNoticias()
+
   }
 
+  carregarNoticias(){
+
+    this.noticiaService.getNoticias().subscribe((noticiasRecebidas: Noticias []) => {
+
+      this.listaNoticias = noticiasRecebidas
+
+      console.log(this.listaNoticias);
+    })
+  }
+
+  
 }
   
